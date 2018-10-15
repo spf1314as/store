5a94048083e2a201d8b349df95d13ac479d65815
shenpengfei <shenpengfei@xiaomi.com>
2018-10-15 11:09:07 +0800

    增加debounce

--- src/throltt.js
+++ src/throltt.js
@@ -1,9 +1,17 @@
+/*** 函数节流  规定时间内执行性一次
+ * @param fun
+ * @param delay
+ * @returns {Function}
+ */
+
+
+
 function throttle(fun, delay) {
     var last, deferTimer;
     return function () {
         var that = this,
-          _args = arguments,
-          now = +new Date();
+            _args = arguments,
+            now = +new Date();
         if (last && now < last + delay) {
             clearTimeout(deferTimer)
             deferTimer = setTimeout(function () {
@@ -21,8 +29,33 @@ function ajax(content) {
 }
 
 var throttleAjax = throttle(ajax, 2000);
-setInterval(function () {
-    throttleAjax()
-    
-},1000)
+// setInterval(function () {
+//     throttleAjax()
+//
+// },1000)
+
+/*
+** 函数防抖
+* @params fn
+* @params delay
+* @return  {Function}
+* */
+
+function  debounce(fn ,delay = 3000){
+    let deferTimer = null;
+    return function (...res) {
+        let that = this;
+        deferTimer && clearTimeout(deferTimer);
+        deferTimer = setTimeout(function () {
+            fn.apply(that,res)
+        },delay)
+
+    }
+}
+var debounceAjax = debounce(ajax);
+
+setInterval(() => {
+    debounceAjax()
+},2000)
+
 
